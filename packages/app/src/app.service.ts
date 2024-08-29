4a70f45f2ae014e9488e3981e5405f5be009e9e8
coppla <65837127+shin-hama@users.noreply.github.com>
2024-08-29 00:25:53 +0000

    FEAT: Notion API を使用してページを作成するサンプル

--- packages/app/src/app.service.ts
+++ packages/app/src/app.service.ts
@@ -1,8 +1,26 @@
 import { Injectable } from '@nestjs/common';
+import { createPage } from "./notion/create-page"
+import { ConfigService } from '@nestjs/config';
+import { Client } from '@notionhq/client';
+
 
 @Injectable()
 export class AppService {
+  private notion: Client
+  constructor(
+    private configService: ConfigService
+  ) {
+    this.notion = new Client({
+      auth: this.configService.get<string>("NOTION_TOKEN")
+    });
+  }
   getHello(): string {
-    return 'Hello World!';
+    try {      
+      createPage(this.notion, "b08e0db3e6584e6887fed9786c62c153")
+      return 'Hello World!';
+    }
+    catch (e){
+      console.log(e)
+    }
   }
 }
---
+++ packages/app/src/notion/create-page.ts
@@ -0,0 +1,67 @@
+import { Client } from '@notionhq/client';
+
+
+export const createPage = async (notion: Client, database_id: string) => {
+  const response = await notion.pages.create({
+    parent: {
+      type: 'database_id',
+      database_id,
+    },
+    properties: {
+      title: {
+        title: [
+          {
+            text: {
+              content: 'Hello, World!',
+            },
+          },
+        ],
+      },
+      Color: {
+        select: {
+          name: 'Red',
+        },
+      },
+    },
+    children: [
+      {
+        object: 'block',
+        callout: {
+          rich_text: [
+            {
+              text: {
+                content: 'Original Text on red background',
+              },
+            },
+          ],
+          color: 'red_background',
+        },
+      },
+      {
+        object: 'block',
+        callout: {
+          rich_text: [
+            {
+              text: {
+                content: 'Original Text on red',
+              },
+            },
+          ],
+          color: 'red',
+        },
+      },
+      {
+        object: 'block',
+        paragraph: {
+          rich_text: [
+            {
+              text: {
+                content: 'Any comment',
+              },
+            },
+          ],
+        },
+      },
+    ],
+  })
+}
