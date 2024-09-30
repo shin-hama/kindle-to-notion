928c5cbe43a4fb31422da6cec94f0dcbe843ad0a
shin-hama <htysivista@gmail.com>
2024-10-01 07:39:07 +0900

    MAINT: change settings

--- supabase/functions/test/index.ts
+++ supabase/functions/test/index.ts
@@ -3,30 +3,27 @@
 // This enables autocomplete, go to definition, etc.
 
 // Setup type definitions for built-in Supabase Runtime APIs
-import { Hono } from "jsr:@hono/hono";
-import { createClient } from "jsr:@supabase/supabase-js@2";
+import "jsr:@supabase/functions-js/edge-runtime.d.ts"
 
-// change this to your function name
-const app = new Hono();
+console.log("Hello from Functions!")
 
-app.post("/", async (c) => {
-  const test = await c.req.json();
-  console.log(test);
-  createClient(
-    Deno.env.get("SUPABASE_URL") ?? "",
-    Deno.env.get("SUPABASE_SERVICE_ROLE_KEY") ?? "",
-  );
-  return c.text("Hello from hono-server!");
-});
+Deno.serve(async (req) => {
+  const data = {
+    message: `Hello name!`,
+  }
 
-Deno.serve(app.fetch);
+  return new Response(
+    JSON.stringify(data),
+    { headers: { "Content-Type": "application/json" } },
+  )
+})
 
 /* To invoke locally:
 
   1. Run `supabase start` (see: https://supabase.com/docs/reference/cli/supabase-start)
   2. Make an HTTP request:
 
-  curl -i --location --request POST 'http://127.0.0.1:54321/functions/v1/notion-wrapper' \
+  curl -i --location --request POST 'http://127.0.0.1:54321/functions/v1/test' \
     --header 'Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZS1kZW1vIiwicm9sZSI6ImFub24iLCJleHAiOjE5ODM4MTI5OTZ9.CRXP1A7WOeoJeXxjNni43kdQwgnWNReilDMblYTn_I0' \
     --header 'Content-Type: application/json' \
     --data '{"name":"Functions"}'
