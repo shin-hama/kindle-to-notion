4554d33fef13031732969f7f09157448d89a999f
shin-hama <htysivista@gmail.com>
2024-11-03 12:02:01 +0900

    CI: change steps order

--- packages/mod/build_npm.ts
+++ packages/mod/build_npm.ts
@@ -6,7 +6,7 @@ await build({
   entryPoints: ["../../supabase/functions/api/client.ts"],
   outDir: "../apiClient",
   packageManager: "pnpm",
-  skipNpmInstall: false,
+  skipNpmInstall: Deno.env.get("CI") === "true",
   shims: {
     deno: true,
   },
