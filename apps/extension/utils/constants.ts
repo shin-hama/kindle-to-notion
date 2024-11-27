7cd70be18ec3487a5276c5c612b3d2d75cbe845c
shin-hama <htysivista@gmail.com>
2024-11-28 06:43:47 +0900

    FIX: update content script matches for Amazon regions

--- apps/extension/utils/constants.ts
+++ apps/extension/utils/constants.ts
@@ -2,7 +2,9 @@ import { AmazonRegions } from "./region";
 
 export const contentScriptMatches = import.meta.env.DEV
   ? ["*://read.amazon.co.jp/notebook*", "http://localhost/*"]
-  : Object.values(AmazonRegions).map((region) => `*://*.${region.hostname}/*`);
+  : Object.values(AmazonRegions).map((region) =>
+    `*://read.${region.hostname}/*`
+  );
 
 export const cookiesPermissions = import.meta.env.DEV
   ? ["*://localhost:54321/*"]
